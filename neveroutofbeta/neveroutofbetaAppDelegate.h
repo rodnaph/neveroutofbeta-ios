811505e5aace130640d7754f8c9dccd5e12dca43
rod <rod@pu-gh.com>
2011-07-15 09:11:15 +0100

    Initial import

--- neveroutofbeta/neveroutofbetaAppDelegate.h
+++ neveroutofbeta/neveroutofbetaAppDelegate.h
@@ -1,17 +1,7 @@
-//
-//  neveroutofbetaAppDelegate.h
-//  neveroutofbeta
-//
-//  Created by Rod on 15/07/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
 
 @interface neveroutofbetaAppDelegate : NSObject <UIApplicationDelegate> {
-
+    IBOutlet UIWindow *window;
+    UINavigationController *navigationController;
 }
-
-@property (nonatomic, retain) IBOutlet UIWindow *window;
 
 @end
